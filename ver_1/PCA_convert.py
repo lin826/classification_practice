Correct the PCA calculation

ver_1/PCA_convert.py
@@ -2,60 +2,39 @@ import numpy as np
 from bmp_readin import get_data,save_item,get_item
 from numpy import linalg
 
-DATA = get_data()
-NUM_CLASS = len(DATA)
-NUM_TRAIN = [len(DATA[i]) for i in range(NUM_CLASS)]
-TARGET_DIM = 2
-def convert_to_X(data):
-    tmp = list()
-    for i in DATA: # Beyond Class
-        for j in i: # get all pictures
-            tmp.append([[a] for a in j])
-    return np.array(tmp)
 
-def get_covariance_matrix(X):
-    N = len(X)
-    # Get mean of x
-    mean = X.mean()
-    # Get covariance matrix
-    S = (X[0]-mean)*np.transpose(X[0]-mean)
-    for i in range(1,N):
-        S = np.add((X[i]-mean)*np.transpose((X[i]-mean)), S)
-    return S/N
-
-def get_X():
-    # X = convert_to_X(DATA)
-    # save_item(X,"../tmp/X.npy")
-    X = get_item("../tmp/X.npy")
-    return X
-
-def get_S():
-    # S = get_covariance_matrix(X)
-    # save_item(S,"../tmp/S_matrix.npy")
-    S = get_item("../tmp/S_matrix.npy")
-    return S
+def reduce_simpliest_PCA(i):
+    global DATA, TARGET_DIM
+    DATA = get_data()
+    TARGET_DIM = int(i)
+    print("Start PCA...")
+    formulate_target()
+    get_eigenvector()
 
 def get_eigenvector():
+    # Get data X
+    ori_dim = DATA.shape[2]
+    X = DATA.reshape([-1,ori_dim])
+
+    # Get covariance matrix
+    S = np.cov(X.T)
+
     # Get eigenvalues and eigenvectors
-    # w, v = linalg.eig(S)
-    # save_item(w,"../tmp/w_eigenvalue.npy")
-    # save_item(v,"../tmp/v_eigenvactors.npy")
-    w = get_item("../tmp/w_eigenvalue.npy")
-    v = get_item("../tmp/v_eigenvactors.npy")
-    # Get the most important two
+    w, v = linalg.eig(S)
+    save_item(w,"../tmp/w_eigenvalue.npy")
+    save_item(v,"../tmp/v_eigenvactors.npy")
+    # # w = get_item("../tmp/w_eigenvalue.npy")
+    # # v = get_item("../tmp/v_eigenvactors.npy")
+
+    # Get the most important ev
     x_pair = [(np.abs(w[i]), v[:,i]) for i in range(len(w))]
     x_pair.sort(key=lambda x: x[0], reverse=True)
-    x_vector = np.array([x_pair[0][1],x_pair[1][1]]).transpose()
-    return x_vector
+    x_vector = np.array([x_pair[i][1] for i in range(TARGET_DIM)]).transpose()
 
-def reduce_dim(i,ev):
-    i = np.array(i)
-    output = [0 for k in range(TARGET_DIM)]
-    for e in range(TARGET_DIM):
-        eigen_vec = np.array([ev[:,e]]).transpose()
-        output[e] = np.dot(i,eigen_vec)
-    output = normalize(output)
-    return output.flatten()
+    # Normalize the result
+    X = X.dot(x_vector)
+    X = normalize(X)
+    save_item(X,'../tmp/data_based.npy')
 
 def normalize(v):
     norm=np.linalg.norm(v)
@@ -68,29 +47,16 @@ def normalize(v):
                       RuntimeWarning)
     return v / std_dev
 
-def reduce_simpliest_PCA():
-    ev = get_eigenvector()
-    a = np.array([[[0.0 for k in range(TARGET_DIM)] for j in range(len(DATA[i]))] for i in range(len(DATA))])
-    for i in range(len(DATA)): # Beyond Class
-        for j in range(len(DATA[i])): # get all pictures
-            a[i][j] = reduce_dim(DATA[i][j],ev)
-    print(a.shape)
-    save_item(a,"../tmp/DATA_reduced.npy")
-
-def formulate_data(data):
+def formulate_target():
+    N = DATA.shape[0]* DATA.shape[1]
     target_list = list()
-    based_list = list()
-    for a in range(len(data)):
+    for a in range(len(DATA)):
         target = np.array([0,0,0])
         target[a] = 1
-        for item in data[a]:
+        for item in DATA[a]:
             target_list.append(target)
-            based_list.append([i for i in item])
-    based_list = np.array(based_list)
     target_list = np.array(target_list)
-    save_item(based_list,'../tmp/data_based.npy')
     save_item(target_list,'../tmp/data_target.npy')
 
-# reduce_simpliest_PCA()
-# DATA = get_item("../tmp/DATA_reduced.npy")
-# formulate_data(DATA)
+if __name__ == '__main__':
+    reduce_simpliest_PCA(sys.argv[1])

ver_1/main.py
@@ -1,28 +1,29 @@
-import numpy
+import numpy as np
 from model import PDM, PGM
 from bmp_readin import save_item,get_item
-from PCA_convert import reduce_simpliest_PCA,formulate_data
+from PCA_convert import reduce_simpliest_PCA
+from draw import draw_decision_region
 
 # data_size upto 3000
 s = {"map_size":1081,"dim":2,"data_size":2400,
-    "batch_size":1,"iter":100,"k_folder":0,
+    "batch_size":1,"iter":10,"k_folder":0,"PCA_reset":1,
     "x_train":"../tmp/data_based.npy","t_train":"../tmp/data_target.npy"}
 
 def model_init():
     global Ground_x,Ground_t,settings
     Ground_x = get_item(s["x_train"])
     Ground_t = get_item(s["t_train"])
-    Ground_x = numpy.array(Ground_x)
-    Ground_t = numpy.array(Ground_t)
+    Ground_x = np.array(Ground_x)
+    Ground_t = np.array(Ground_t)
     settings = s
     print('Finish initializing')
 
 def model_setting(k):
     global Ground_x,Ground_t,Train_x,Train_t,Test_x,Test_t
     n = len(Ground_x)
-    indices = numpy.asarray(range(n), dtype=numpy.int32)
+    indices = np.asarray(range(n), dtype=np.int32)
     n_train = s['data_size']
-    numpy.random.shuffle(indices)
+    np.random.shuffle(indices)
     if s['k_folder'] == 0:
         Train_x = Ground_x[indices[:n_train]]
         Train_t = Ground_t[indices[:n_train]]
@@ -46,10 +47,22 @@ def model_setting(k):
     print('Train_t',Train_t.shape)
     return 0
 
-reduce_simpliest_PCA()
-formulate_data(get_item("../tmp/DATA_reduced.npy"))
+if(s["PCA_reset"]> 0):
+    reduce_simpliest_PCA(s["dim"])
 
 model_init()
-model_setting(0)
-# m = PDM(Train_x,Train_t,Test_x,Test_t)
-m = PGM(Train_x,Train_t,Test_x,Test_t)
+
+if(s['k_folder']==0):
+    model_setting(0)
+
+    pdm = PDM()
+    pdm.run(Train_x, Train_t,s['iter'])
+    e1=pdm.eval(Test_x, Test_t)
+
+    pgm = PGM()
+    pgm.run(Train_x, Train_t)
+    e2=pgm.eval(Test_x, Test_t)
+
+    draw_decision_region(Test_x,Test_t,[pdm,pgm],[e1,e2])
+# else:
+#     print('K-folder is not implemented')

ver_1/model.py
@@ -1,11 +1,15 @@
 import numpy as np
+from bmp_readin import save_item,get_item
 
 def cal_softmax(a):
     e = np.exp(a)
     return e / e.sum(axis=1)[:, np.newaxis]
 
 class PGM:
-    def __init__(self,x,t,test_x,test_t):
+    def __init__(self):
+        pass
+
+    def run(self,x,t):
         m = x.shape[1] # 2
         k = t.shape[1] # 3
         n = x.shape[0]
@@ -15,10 +19,14 @@ class PGM:
 
         for j in range(k):
             qualified_data = x[t[:, j] == 1]
-            p[j] = len(qualified_data) / n
-            mean[j] = np.mean(qualified_data, axis=0)
-            dif = qualified_data - mean[j]
-            sig += p[j] * dif.T.dot(dif) / len(qualified_data)
+            if(len(qualified_data)==0):
+                p[j] = 0
+                continue
+            else:
+                p[j] = len(qualified_data) / n
+                mean[j] = np.mean(qualified_data, axis=0)
+                dif = qualified_data - mean[j]
+                sig += p[j] * dif.T.dot(dif) / len(qualified_data)
 
         sig_ = np.linalg.pinv(sig)
 
@@ -27,23 +35,35 @@ class PGM:
         for i in range(k):
             self.w[i] = np.dot(sig_,mean[i])
             self.om[i] = (-1.0/2)*mean[i].T.dot(sig_.dot(mean[i])) + np.log(p[i])[0]
-        self.get_result(x,t)
-
-        y = cal_softmax(test_x.dot(self.w.T) + np.tile(self.om.T,(len(test_x),1)))
-        acc = float(np.equal(y.argmax(axis=1), test_t.argmax(axis=1)).sum()) / len(test_t)
-        print ('Accuracy: ', acc)
-
-    def get_result(self,x,t):
-        n = len(x)
-        k = t.shape[1]
-        # y = np.zeros([n,k])
-        y = cal_softmax(x.dot(self.w.T) + np.tile(self.om.T,(n,1)))
+        y = self.predict(x)
         acc = float(np.equal(y.argmax(axis=1), t.argmax(axis=1)).sum()) / len(t)
         print ('acc', acc)
 
-class PDM_basic:
+    def eval(self,test_x,test_t):
+        y = self.predict(test_x)
+        acc = float(np.equal(y.argmax(axis=1), test_t.argmax(axis=1)).sum()) / len(test_t)
+        print (self.get_title()+'Error rate: ', 1-acc)
+        return 1-acc
+
+    def predict(self,x):
+        return cal_softmax(x.dot(self.w.T) + np.tile(self.om.T,(len(x),1)))
+
+    def get_title(self):
+        return "Multi-class Probabilistic Generative Model"
+
+    def set_setting(self,data):
+        self.w = data[0]
+        self.om = data[1]
+
+class PDM:
     def __init__(self):
         pass
+
+    def eval(self,test_x, test_t):
+        y = self.predict(test_x)
+        acc = float(np.equal(y.argmax(axis=1), test_t.argmax(axis=1)).sum()) / len(test_t)
+        print (self.get_title()+'Error rate: ', 1-acc)
+        return 1-acc
 
     def cal_activations(self, p):
         a = p.dot(self.W.T)
@@ -61,24 +81,6 @@ class PDM_basic:
         x = self.phi
         # Set y
         self.y = cal_softmax(self.phi.dot(self.W.T))
-
-        # # Calculate gradient
-        # grad = np.zeros([K, M])
-        # for j in range(K):
-        #     for n in range(N):
-        #         grad[j, :] += (self.y[n,j] - self.t[n,j]) * x[n]
-        # self.gradient = grad.flatten()
-        #
-        # # Calculate Hessian matrix
-        # I = np.identity(K)
-        # H = np.zeros([K*M, K*M])
-        # for j in range(K):
-        #     for k in range(K):
-        #         D_wjk = np.zeros([M, M])
-        #         for n in range(N):
-        #             D_wjk += self.y[n,k] * (I[k,j] - self.y[n,j]) * x.T.dot(x)
-        #         H[j*(M):(j+1)*M, (k)*M:(k+1)*M] = D_wjk
-        # self.hessian = H
 
         # Set gradient
         E = np.zeros((K, M))
@@ -104,7 +106,7 @@ class PDM_basic:
         w_new = w_old - np.dot(H_,self.gradient)
         self.W = w_new.reshape([self.K, self.M])
 
-    def do_IRLS(self,times,x,t):
+    def run(self,x,t,times):
         self.M = len(x[0])+1
         self.K = len(t[0])
         self.W = np.zeros((self.K , self.M))
@@ -120,18 +122,15 @@ class PDM_basic:
                 self.phi = self.get_phi(x[indices[begin:end]])
                 self.t = t[indices[begin:end]]
                 self.update()
-
-            phi = self.get_phi(x)
-            y = cal_softmax(phi.dot(self.W.T))
+            y = self.predict(x)
             acc = float(np.equal(y.argmax(axis=1), t.argmax(axis=1)).sum()) / len(t)
             print ('acc', acc)
 
-class PDM:
-    def __init__(self,x,t,test_x,test_t):
-        self.basic = PDM_basic()
-        self.basic.do_IRLS(10,x,t)
 
-        phi = self.get_phi(test_x)
+    def predict(self,x):
+        phi = self.get_phi(x)
         y = cal_softmax(phi.dot(self.W.T))
-        acc = float(np.equal(y.argmax(axis=1), test_t.argmax(axis=1)).sum()) / len(t)
-        print ('Accuracy: ', acc)
+        return y
+
+    def get_title(self):
+        return "Multi-class Probabilistic Discriminative Model"